74bb7d75b9b099122fff49da2a2339fabab85326
henorch <50269830+henorch@users.noreply.github.com>
2024-07-30 18:07:29 +0100

    Update slider.js

--- src/slide/slider.js
+++ src/slide/slider.js
@@ -1,5 +1,5 @@
 var Slider = document.getElementById('mainSlider');
 
-Slider.style.backgroundImage = "url('../henOrch/assets//images/images.jpeg')";
+Slider.style.backgroundImage = "url('../henorch/assets//images/images.jpeg')";
 Slider.style.backgroundSize = 'cover';
 Slider.style.backgroundPosition = 'center'
